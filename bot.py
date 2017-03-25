e50663fd9eae99d3cc80759008abfb9c5f016e0f
Andrey Bakhmat <a.bakhmat@gmail.com>
2017-03-25 14:19:53 +0300

    добалил функцию stop (placeholder)

--- bot.py
+++ bot.py
@@ -6,6 +6,7 @@ def greet_user(bot, update):
 	#print('Вызван /start')
 	bot.sendMessage(update.message.chat_id, text='Давай общаться!')
 
+
 def talk_to_me(bot, update):
     print(update.message.text)
     bot.sendMessage(update.message.chat_id, update.message.text)
@@ -15,6 +16,9 @@ def main():
 
 	dp = updater.dispatcher
 	dp.add_handler(CommandHandler("start", greet_user))
+	dp.add_handler(CommandHandler("stop", greet_user))
+
+
 	dp.add_handler(MessageHandler([Filters.text], talk_to_me))
 
 	dp.add_error_handler(show_error)
